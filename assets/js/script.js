364219c73efda9da7b0f125f07158beea394386c
ManzXD <aimanmk89@gmail.com>
2024-07-21 15:29:55 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -534,7 +534,7 @@ const skipNext = function () {
   changePlaylistItem();
 }
 
-playerSkipNextBtn.addEventListener("overlay", skipNext);
+playerSkipNextBtn.addEventListener("click", skipNext);
 
 
 
